a780d72ebd0cb2cad8de380e184b071035bec7b5
Harshhh2003 <133561905+Harshhh2003@users.noreply.github.com>
2024-09-24 16:11:39 +0530

    sum of digits using recursion

---
+++ recursion5.java
@@ -0,0 +1,17 @@
+// given an integer find out the sum of its digitsnusingb recursion
+public class recursion5{
+    static int sumofdigits(int n){
+        // base case 
+        if(n >=0 && n<=9){
+            return n;
+        }
+       int smallans = sumofdigits(n/10);
+       int ans = smallans + n%10;
+       return ans ;
+
+    }
+    public static void main(String[] args) {
+      System.out.println("the ans is   :   "   +   sumofdigits(22222));
+        
+    }
+}
