4eb1ef0fb8bd75fafa79f24ebac151852eb25df3
Harshhh2003 <133561905+Harshhh2003@users.noreply.github.com>
2024-09-28 19:24:29 +0530

    finding max value in an array using recursion

---
+++ recursiorecursion11.java/recursion11.java
@@ -0,0 +1,15 @@
+public class recursion11 {
+    public static int maxvalue(int[] array, int index) {
+        if (index >= array.length - 1) {
+            return array[index];
+        } else {
+            return Math.max(array[index], maxvalue(array, index + 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {23, 70, 32, 21, 10};
+        int max = maxvalue(array, 0); // Directly calling maxvalue
+        System.out.println("Maximum value: " + max);
+    }
+}
